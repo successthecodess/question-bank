926a8182d408dcb2ad5c3be7872242a7ac2661a2
successthecodess <successthecodess@gmail.com>
2025-11-08 12:04:52 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: '/question-bank/',
   plugins: [react(),
     tailwindcss()
   ],
